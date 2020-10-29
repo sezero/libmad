23c36bc74f8b03f8d3c745ac264028dbdefb9b7c
Ozkan Sezer <sezeroz@gmail.com>
2020-10-29 08:28:02 +0300

    added powerpc asm patch from RedHat:
    https://bugzilla.rpmfusion.org/show_bug.cgi?id=730
    libmad outputs white noise due to incorrect asm constraints
    dwmw2@infradead.org
    2009-07-19 14:09:50 CEST
    
    The constraints lacked an earlyclobber on the outputs, so gcc was free to use
    the same register for input and output. This version is faster than the simple
    fix.

--- fixed.h
+++ fixed.h
@@ -379,8 +379,8 @@ mad_fixed_t mad_f_mul_inline(mad_fixed_t x, mad_fixed_t y)
        asm ("addc %0,%2,%3\n\t"  \
 	    "adde %1,%4,%5"  \
 	    : "=r" (lo), "=r" (hi)  \
-	    : "%r" (lo), "r" (__lo),  \
-	      "%r" (hi), "r" (__hi)  \
+	    : "0" (lo), "r" (__lo), \
+	      "1" (hi), "r" (__hi) \
 	    : "xer");  \
     })
 #  endif
